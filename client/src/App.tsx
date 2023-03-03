210040fc04f3f3e8dc34d779cf747477285209cd
Connor Dalrymple <packetson2@gmail.com>
2023-03-03 10:30:31 -0600

    add front end files

---
+++ client/src/App.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+import './App.css'
+
+function App() {
+
+  let [messages, setMessages] = React.useState<Array<string>>([]);
+  let textBox = React.useRef(null);
+  //React.useEffect()
+
+  async function sendMessage(message: string) {
+    await fetch("http://localhost:8000/connect/defRoom", {
+      method: "POST",
+      headers: {
+        'Content-Type' : 'text/plain'
+      },
+      body: message
+    }).then((ret) => {
+      console.log(ret.ok?"Message sent ok": "error sending message")
+    })
+  }
+
+  function submit(key:React.KeyboardEvent) {
+    if (key.key === "Enter") {
+      sendMessage("click")
+    }
+  }
+
+  return <>
+    <input ref={textBox} onKeyDown={submit}></input>
+    <div>
+      {messages.map((msg) => { return (<div>{msg}</div>)})}
+    </div>
+  </>
+}
+
+export default App
