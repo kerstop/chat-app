06c496f42125d08bfd1b9d3e69e66579218cd84b
kerstop <packetson2@gmail.com>
2024-03-23 13:39:29 -0500

    found some changes on my hard drive

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,51 +1,57 @@
-import * as React from 'react'
-import './App.css'
+import * as React from "react";
+import "./App.css";
 
 function App() {
-
-  let [messages, setMessages] = React.useState<Array<string>>(["test"]);
-  let textBox: React.MutableRefObject<null | HTMLInputElement> = React.useRef(null);
+  let [messages, setMessages] = React.useState<Array<string>>([]);
+  let textBox: React.MutableRefObject<null | HTMLInputElement> =
+    React.useRef(null);
 
   function append_message(msg: string) {
     messages.push(msg);
-    setMessages(messages.slice())
+    setMessages([...messages]);
   }
 
   React.useEffect(() => {
-    let message_stream = new WebSocket("ws://localhost:8080/connect/defRoom")
+    let message_stream = new WebSocket("ws://localhost:8080/connect/defRoom");
     message_stream.onmessage = (event) => {
       console.log(event.data);
       append_message(event.data);
       console.log(messages);
-    }
-  }, [])
+    };
+  }, []);
 
   async function sendMessage(message: string) {
     await fetch("http://localhost:8080/connect/defRoom", {
       method: "POST",
       headers: {
-        'Content-Type' : 'application/json',
-        'Access-Control-Allow-Origin': "*",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
       },
-      body: JSON.stringify(message)
+      body: JSON.stringify(message),
     }).then((ret) => {
-      console.log(ret.ok?"Message sent ok": "error sending message")
-    })
+      console.log(ret.ok ? "Message sent ok" : "error sending message");
+    });
   }
 
-  function submit(key:React.KeyboardEvent) {
+  function submit(key: React.KeyboardEvent) {
     if (key.key === "Enter" && textBox.current != null) {
-      sendMessage(textBox.current.value)
+      sendMessage(textBox.current.value);
       textBox.current.value = "";
     }
   }
 
-  return <>
-    <input ref={textBox} onKeyDown={submit}></input>
-    <div>
-      {messages.map((msg,i) => { return (<div key={i}>{msg}</div>)})}
+  return (
+    <div className="app">
+      <div className="message-view">
+        {messages.map((msg, i) => {
+          return <div className="message" key={i}>{msg}</div>;
+        }).reverse()}
+      </div>
+      <div className="footer">
+        <input className="text-input" ref={textBox} onKeyDown={submit}></input>
+      </div>
     </div>
-  </>
+  );
 }
 
-export default App
+export default App;
